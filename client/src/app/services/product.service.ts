eb4aa88f81e41a057adbebf8a84652648b3741a3
ahmed hamed <ahmed.hd1011@gmail.com>
2024-03-05 16:11:06 +0200

    edit authorization

--- client/src/app/services/product.service.ts
+++ client/src/app/services/product.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IProduct } from '../models/Product';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  baseUrl = "https://localhost:7012/api";
+  baseUrl = "https://localhost:44335/api";
+
+  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0OSIsImp0aSI6IjZmMzUyZGY0LTU0YzQtNGI5My1iYTM0LWYzNjA2Y2NlYWQzNiIsImVtYWlsIjoidGVzdDlAZ21haWwuY29tIiwidWlkIjoiNTE3MDhhYzYtODIzZC00YzIzLTk5MjgtYzJmZmYyOWY0NWUxIiwicm9sZSI6IlVzZXIiLCJleHAiOjE3MTE2OTkwODgsImlzcyI6Im15c3RvcmUiLCJhdWQiOiJteXN0b3JlIn0.kanT1DYolDDZOF6-Mnce2K99nlzCnXJUS3QH3r661q8";
 
   constructor(private http: HttpClient) { }
-
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(this.baseUrl + '/products');
+    const headers = new HttpHeaders ({
+      "Authorization" : "bearer " + this.token
+    });
+    return this.http.get<IProduct[]>(this.baseUrl + '/products', {headers});
   }
 }
